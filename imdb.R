b7cebc6867bc82ee13e460b3f4d72cb2f4a741f9
J Steven Raquel <jsteven.raquel8@gmail.com>
2016-09-14 22:20:02 -0700

    Update imdb.R

--- imdb.R
+++ imdb.R
@@ -23,26 +23,26 @@ eighties <- subset(movies, title_year >= 1980 & title_year < 1990)
 
 # Boxplots of rating of movies of each year from 2010 on
 tens_plot <- ggplot(tens, aes(x = factor(title_year), y = imdb_score)) + geom_boxplot()
-tens_plot <- tens_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-tens_plot <- tens_plot + labs(title = "2010s", x = "Year of Release", y = "Rating")
+tens_plot <- tens_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_continuous(breaks = seq(0,10,2))
+tens_plot <- tens_plot + labs(title = "2010s Films", x = "Year of Release", y = "IMDB Rating")
 tens_plot
 
 # for 2000-2010
 zeroes_plot <- ggplot(zeroes, aes(x = factor(title_year), y = imdb_score)) + geom_boxplot()
-zeroes_plot <- zeroes_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-zeroes_plot <- zeroes_plot + labs(title = "2000s", x = "Year of Release", y = "Rating")
+zeroes_plot <- zeroes_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_continuous(breaks = seq(0,10,2))
+zeroes_plot <- zeroes_plot + labs(title = "2000s Films", x = "Year of Release", y = "IMDB Rating")
 zeroes_plot
 
 # for 1990-2000
 nineties_plot <- ggplot(nineties, aes(x = factor(title_year), y = imdb_score)) + geom_boxplot()
-nineties_plot <- nineties_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-nineties_plot <- nineties_plot + labs(title = "1990s", x = "Year of Release", y = "Rating")
+nineties_plot <- nineties_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_continuous(breaks = seq(0,10,2))
+nineties_plot <- nineties_plot + labs(title = "1990s Films", x = "Year of Release", y = "IMDB Rating")
 nineties_plot
 
 # for 1980-1990
 eighties_plot <- ggplot(eighties, aes(x = factor(title_year), y = imdb_score)) + geom_boxplot()
-eighties_plot <- eighties_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-eighties_plot <- eighties_plot + labs(title = "1980s", x = "Year of Release", y = "Rating")
+eighties_plot <- eighties_plot + theme(legend.position = "none") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_y_continuous(breaks = seq(0,10,2))
+eighties_plot <- eighties_plot + labs(title = "1980s Films", x = "Year of Release", y = "IMDB Rating")
 eighties_plot
 
 # Collecting all four plots together
